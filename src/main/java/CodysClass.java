617d8dfea8e7ae8d39b6448503db5e2f5b7199ae
Ralph M <102826377+Rafagithub135@users.noreply.github.com>
2023-01-20 14:16:25 -0500

    Initial commit

---
+++ src/main/java/CodysClass.java
@@ -0,0 +1,24 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class CodysClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("Please enter a string:");
+
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
+        if (StringUtils.isNumeric(input)) {
+            System.out.println(input + " is a number...");
+        } else {
+            System.out.println(input + " is not a number...");
+        }
+        System.out.println("Flipped Case: " + StringUtils.swapCase(input));
+        System.out.println("Reversed: " + StringUtils.reverse(input));
+
+
+    }
+}
